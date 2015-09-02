2ed8a8fcea7ef2cd645cfa5432fd882b6b8843b7
dolphz <dolphz@naver.com>
2015-09-02 13:46:17 +0900

    [Sep02 Lec1 example]
    문자열 예제

---
+++ PLec01/PLec01/PLec01.py
@@ -0,0 +1,47 @@
+﻿#print("Here are the numbers!" + "The first is {0:d} The second is {1:4d} ".format(7,8))
+
+#salary = input("Please enter your salary : ")
+#bonus = input("Please enter your bonus : ")
+##payCheck = salary + bonus
+#payCheck = float(salary) + float(bonus)
+#print(payCheck)
+
+#print(type(payCheck))
+
+#print("한글")
+
+random = 2.3432223423
+round(random,3)
+
+#name = "minkipang"
+#print(name[0])
+#print(name[-1])
+#print(name[-2])
+#print(name[-3])
+#print(name[-4])
+
+#info = "201111261minkipang"
+#sid = info[:9]
+#sname = info[9:]
+#print(sid)
+#print(sname)
+
+#a = "I eat %s apples." % "five" #일반 문자열 포맷 출력
+#b = "I eat %-10s apples." % "five" #왼쪽 정렬
+#c = "I eat %10s apples." % "five" #오른쪽 정렬
+#d = "Error is %d%%" % 98
+
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+
+#iput = input("종료할까요? : ")
+#if(iput[0] == 'Y')
+#{
+#    print(iput.lower()) 
+#}
+#else
+#{
+#    print(iput.upper())
+#}
